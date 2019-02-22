bd3febfe5fcfbb3eb1e12ac435c081aeb3ddbba2
mumu <mumu@example.com>
2019-02-22 16:06:50 +0800

    '基本显示'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div>
+      <FooterGuide/>
+      <router-view/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    components:{
+      FooterGuide
+    }
   }
-}
 </script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
